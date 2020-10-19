5b031c99745e63d7061ab5e4d0d86985e8308d9b
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-10-19 14:18:22 -0500

    reducers updated for redux

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,8 +10,16 @@ import {
   CLEAR_CART,
   TOGGLE_CART
 } from './actions';
+
+const initialState = {
+  products: [],
+  categories: [],
+  cart: [],
+  currentCategory: '',
+  cartOpen: false,
+}
   
-export const reducer = (state, action) => {
+export const reducer = (state = initialState, action) => {
     switch (action.type) {
       // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
       case UPDATE_PRODUCTS:
@@ -86,6 +94,4 @@ export const reducer = (state, action) => {
     }
 };
 
-export function useProductReducer(initialState) {
-    return useReducer(reducer, initialState);
-}
+export default reducer;
